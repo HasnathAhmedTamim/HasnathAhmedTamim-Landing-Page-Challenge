2ea08350c048da62a8f24a5f84d1698f3709c3f5
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2024-05-19 23:52:48 +0600

    vite setup , tailwindcss setup and react router setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    
+      <h1 className='text-4xl font-bold text-centers items-center justify-center flex'>Vite + React app</h1>
+     
+    </>
+  )
+}
+
+export default App
