14946353a18a18ff17efadbb3e9c392208f113b3
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2024-05-20 16:39:42 +0600

    product section create

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,23 @@
+// Import Swiper React components
+// import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+
+
+// import required modules
+// import { Pagination } from "swiper/modules";
+import Test from "../Test/Test";
+const Products = () => {
+  return (
+    <div className="mb-24">
+      <div className="  ">
+        <Test></Test>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
